8d77ec8de8b9d00c284b799b553ab2af52f9e3c5
cmoulliard <cmoulliard@redhat.com>
2024-06-21 16:30:46 +0200

    Fix typo error. Show cm, statefulset
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- .github/scripts/waitFor.sh
+++ .github/scripts/waitFor.sh
@@ -9,7 +9,7 @@ KUBE_NAMESPACE="${3}"
 EXPECTED="${4}"
 EXTRA="${5-}"
 
-retries=30
+retries=10
 until [[ $retries == 0 ]]; do
   actual=$(kubectl get $RESOURCE $NAME -n $KUBE_NAMESPACE $EXTRA 2>/dev/null || echo "Waiting for $RESOURCE/$NAME in namespace $KUBE_NAMESPACE -> $EXPECTED to appear")
   if [[ "$actual" =~ .*"$EXPECTED".* ]]; then
@@ -20,7 +20,7 @@ until [[ $retries == 0 ]]; do
     echo "Waiting for resource \"$RESOURCE/$NAME\" in namespace $KUBE_NAMESPACE ..." 2>&1
     echo "$actual" 2>&1
   fi
-  sleep 10s
+  sleep 15s
   retries=$((retries - 1))
 done
 
